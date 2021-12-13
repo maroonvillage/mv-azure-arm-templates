281f516dc829ed57210c301c49dd0d787147bc32
Cole Parsons <coleparsons@hotmail.com>
2021-12-12 20:14:19 -0800

    added folder for database templates

---
+++ mvdatabases/cosmosdb-dev/deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+az group create --name mv-databases-rg --location eastus
+
+az deployment group create --resource-group mv-databases-rg --template-file template.json --parameters parameters.json

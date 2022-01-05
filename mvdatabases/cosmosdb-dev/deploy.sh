5259540ed6c46e987c0ab7d53047b07083ea6894
Cole Parsons <coleparsons@hotmail.com>
2022-01-04 19:32:44 -0800

    added configuration template

--- mvdatabases/cosmosdb-dev/deploy.sh
+++ mvdatabases/cosmosdb-dev/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 az login
 
-az group create --name mv-databases-rg --location eastus
+az group create --name mv-databases-rg --location westus
 
 az deployment group create --resource-group mv-databases-rg --template-file template.json --parameters parameters.json
--- mvsecurity/keyvault/deploy.sh
+++ mvsecurity/keyvault/deploy.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
+
+#TODO: Add logic to check if login is necessary 
 az login
 
 az group create --name mv-security-rg --location eastus
 
 az deployment group create --resource-group mv-security-rg --template-file template.json --parameters parameters.json
+
+
+
+az group create --name colepsecurity-rg --location westus
+az keyvault create \
+  --name colep-keyvault-wus \
+  --resource-group colepsecurity-rg \
+  --location westus \
+  --enabled-for-template-deployment true
+az keyvault secret set --vault-name colep-keyvault-wus --name "cosmosdbprimarykey" --value "jypdxBXO1xl1u4yaqrFGV0HIcQd1TzS9EeO01ouiLn8UVRBW3zqCea1PVB3caPGf3MI4X76kdsTZQJHuKhMGsw=="
